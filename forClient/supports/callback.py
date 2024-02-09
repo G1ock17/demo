35981fb2f959b2f5c675f06f9ec4653a5ceb56e2
G1ock17 <109388456+G1ock17@users.noreply.github.com>
2024-02-09 17:55:46 +0300

    Add files via upload

--- forClient/supports/callback.py
+++ forClient/supports/callback.py
@@ -1,20 +1,27 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
-from aiogram.types import Message, CallbackQuery, ContentTypes
+from aiogram.types import Message, CallbackQuery, ContentTypes, InputMediaPhoto
 from main import dp, bot
 from data import config
 from database import functions
 from aiogram.types import InlineKeyboardMarkup
 from keyboards import keys
 
+from supports.menu import GiftState, Review
+
+
+class UsedAsk(StatesGroup):
+    asked = State()
+
 
 @dp.callback_query_handler(lambda c: c.data == 'get_gift')
 async def support_request(callback: CallbackQuery):
-    keyboard = keys.share_phone_markup()
     await bot.send_message(chat_id=callback.from_user.id, text=f'{callback.from_user.full_name}\n'
                                                                'У нас для тебя подарок за отзыв. \n\n'
                                                                'Пожалуйста, введи в ответном сообщении свой номер телефона, на него мы отправим бонус!\n'
-                                                               'Или нажми на кнопку «Поделиться номером»', reply_markup=keyboard)
+                                                               'Или нажми на кнопку «Поделиться номером»',
+                           reply_markup=keys.share_phone_markup())
+    await GiftState.WaitingForPhoneNumber.set()
 
 
 @dp.callback_query_handler(lambda c: c.data == 'support_request')
@@ -27,10 +34,6 @@ async def support_request(callback: CallbackQuery):
     await callback.message.answer(text='<b>Выбери пожалуйста категорию обращения: </b>', reply_markup=keyboard,
                                   parse_mode='html')
     await callback.answer()
-
-
-class UsedAsk(StatesGroup):
-    asked = State()
 
 
 @dp.callback_query_handler(lambda c: c.data.startswith('ask_'))
@@ -48,70 +51,85 @@ async def quality_product(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
 
 
-@dp.message_handler(state=UsedAsk.asked)
+@dp.message_handler(content_types=ContentTypes.TEXT, state=UsedAsk.asked)
+async def enter_key1(message: Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['ask'] = message.text
+
+    if message.text == '✅ Подтвердить':
+        functions.add_log('requests')
+        await state.finish()
+        await message.answer(text=f'Готово!\nМы ответим в течение 24 ч.', reply_markup=keys.main_markup(),
+                             parse_mode='html')
+        return
+
+    type_ask = None
+
+    if data['ask_type'] == 'quality_product':
+        type_ask = 'Качество товара'
+    elif data['ask_type'] == 'equipment_product':
+        type_ask = 'Комплектация товара'
+    elif data['ask_type'] == 'any_question':
+        type_ask = 'Другой вопрос'
+
+    await bot.send_message(chat_id=config.GROUP_ID,
+                           text=f"{message.from_user.id}\n{type_ask}\n{data['ask']}")
+
+    await message.answer("Сообщение отправлено! Если вы отправили все хотели, нажмите ✅ Подтвердить")
+
+
+@dp.message_handler(content_types=ContentTypes.PHOTO, state=UsedAsk.asked)
 async def enter_key2(message: Message, state: FSMContext):
     async with state.proxy() as data:
         data['ask'] = message.text
-    if message.text == '✅ Подтвердить':
-        await state.finish()
-        await message.answer(text=f'Готово!\nМы ответим в течение 24 ч.', reply_markup=keys.confirm_markup(),
-                             parse_mode='html')
-    else:
-        if data['ask_type'] == 'quality_product':
-            await bot.send_message(chat_id=config.GROUP_ID,
-                                   text=f"{message.from_user.id}\nКачество товара\n{data['ask']}")
-        if data['ask_type'] == 'equipment_product':
-            await bot.send_message(chat_id=config.GROUP_ID,
-                                   text=f"{message.from_user.id}\nКомплектация товара\n{data['ask']}")
-        if data['ask_type'] == 'any_question':
-            await bot.send_message(chat_id=config.GROUP_ID,
-                                   text=f"{message.from_user.id}\nДругой вопрос\n{data['ask']}")
+    type_ask = None
+    if data['ask_type'] == 'quality_product':
+        type_ask = 'Качество товара'
+    elif data['ask_type'] == 'equipment_product':
+        type_ask = 'Комплектация товара'
+    elif data['ask_type'] == 'any_question':
+        type_ask = 'Другой вопрос'
 
-        await message.answer(text=f'Если вы написали все что хотели нажмите ✅ Подтвердить',
-                             reply_markup=keys.confirm_markup(),
-                             parse_mode='html')
-
-
-@dp.message_handler(content_types=['photo'], state=UsedAsk.asked)
-async def send_photo_to_group(message: Message, state: FSMContext):
+    await state.update_data(screenshot=message.photo[-1])
     photo = message.photo[-1]
-    await bot.send_photo(chat_id=config.GROUP_ID, photo=photo.file_id, caption=f'{message.from_user.id}')
-    # 3-4
-
-    await message.answer("Фотография сохранена! Если вы загрузили все свои фотографии нажмите ✅ Подтвердить")
+    await bot.send_photo(chat_id=config.GROUP_ID, photo=photo.file_id, caption=f'{message.from_user.id}\n{type_ask}')
+    await message.answer('Ваш скриншот успешно отправлен на модерацию ✅')
 
 
-@dp.message_handler(content_types=ContentTypes.TEXT)
+@dp.message_handler(content_types=ContentTypes.ANY)
 async def handle_text_message(message: Message):
     if message.reply_to_message:
         if message.chat.id == config.GROUP_ID:
             user_id = int(message.reply_to_message.text.split('\n')[0])
             await bot.send_message(chat_id=user_id, text=message.text)
+            functions.add_log('answers')
+            functions.add_answer_logs(message.from_user.username, message.text)
+            await bot.send_message(chat_id=config.GROUP_ID, text='Ваш ответ доставленн пользователю')
+            hesoyam = functions.get_answers_logs()
+            print(functions.get_answers_logs())
 
 
-@dp.callback_query_handler(lambda callback: callback.data.startswith(('confirm_', 'rewrite_')))
+@dp.callback_query_handler(lambda callback: callback.data.startswith(('confirm_', 'rewrite_')), state=GiftState.WaitingForPhoneNumber)
 async def handle_confirmation(callback: CallbackQuery, state: FSMContext):
     action, phone_number = callback.data.split('_')
     if action == "confirm":
         functions.add_phone(callback.from_user.id, phone_number)
-        print(phone_number, callback.message.from_user.id)
-        await bot.answer_callback_query(callback.id)
-        await state.update_data(confirm=True)
 
-        await bot.send_message(callback.from_user.id,
+        await bot.answer_callback_query(callback.id)
+        await Review.WaitingForReview.set()
+        await bot.send_message(chat_id=callback.from_user.id, text=
                                'Отлично! А теперь пришли пожалуйста, <b>скриншот</b> твоего отзыва о товаре. '
                                'Мы все проверим и пришлем подарок.\n\n'
-                               'Если отзыв еще не оставлен, то можно это сделать прямо сейчас 😄', parse_mode='html')
+                               'Если отзыв еще не оставлен, то можно это сделать прямо сейчас 😄', reply_markup=keys.main_markup(), parse_mode='html')
 
     elif action == "rewrite":
+        await GiftState.WaitingForPhoneNumber.set()
         await callback.message.answer("Пожалуйста, введите свой номер телефона еще раз.")
 
 
-@dp.callback_query_handler(lambda callback: callback.data in ['bonus_sent', 'reject'], state='*')
+@dp.callback_query_handler(lambda callback: callback.data in ['bonus_sent', 'reject'])
 async def feedback_confirmation_callback(callback: CallbackQuery, state: FSMContext):
     await bot.answer_callback_query(callback.id)
-
-    data = await state.get_data()
     sub_status = functions.get_sub_status(callback.from_user.id)
     phone = functions.get_user_phone(callback.from_user.id)
     if callback.data == 'bonus_sent':
@@ -119,14 +137,14 @@ async def feedback_confirmation_callback(callback: CallbackQuery, state: FSMCont
             functions.switch_sub_status(callback.from_user.id)
             await bot.send_message(callback.from_user.id, '✅ Подарок отправлен\n'
                                                           f'Спасибо за отзыв! Мы все проверили и зачислили вам подарок на номер телефона {phone}')
-            await bot.send_message(callback.from_user.id,
-                                   f'{callback.from_user.full_name} если у вас остались вопросы, жмите на кнопку ниже\n- Написать продавцу')
+            await bot.send_message(chat_id=callback.from_user.id,
+                                   text=f'{callback.from_user.full_name} если у вас остались вопросы, жмите на кнопку ниже\n- Написать продавцу',
+                                   reply_markup=keys.main_markup(), parse_mode='html')
         else:
             await bot.send_message(callback.from_user.id, '❌ Вы уже получали подарок\n'
                                                           'Получить ещё один к сожалению не получится')
     elif callback.data == 'reject':
-        await bot.send_message(callback.from_user.id, '❌ Отзыв отклонен\n'
-                                                      'Сожалеем, но что-то пошло не так. Пожалуйста, попробуйте еще раз.')
-
-    # Сбрасываем состояние
+        await bot.send_message(chat_id=callback.from_user.id,
+                               text='❌ Отзыв отклонен\nСожалеем, но что-то пошло не так. Пожалуйста, попробуйте еще раз.',
+                               reply_markup=keys.main_markup(), parse_mode='html')
     await state.finish()
--- forClient/supports/menu.py
+++ forClient/supports/menu.py
@@ -6,8 +6,48 @@ from main import dp, bot
 from data import config
 from database import functions
 from keyboards import keys
-from filters import IsUser
-from filters import IsAdmin
+import datetime
+import openpyxl
+
+# sheet_index = "https://docs.google.com/spreadsheets/d/11Pp0kiyCf1o_IAiJf2NNbzV4"
+#
+#
+# def connecting_gsheet(sheet_index=1) -> pygsheets.Spreadsheet:
+#     gc = pygsheets.authorize(service_account_file="google/service-account.json")
+#     return gc.open_by_key(config.SHEET_KEY)[sheet_index]
+#
+#
+# def add_data_to_sheet(worksheet, values: list, next_empty_row: int, last_iteration: bool = False, financials=None) -> bool:
+#     try:
+#         if last_iteration and financials:
+#             values.extend(financials)
+#             range_to_update = f"A{next_empty_row}:N{next_empty_row}"
+#         else:
+#             range_to_update = f"A{next_empty_row}:J{next_empty_row}"
+#
+#         worksheet.update_values(range_to_update, [values])
+#         return True
+#     except Exception as e:
+#         print(e)
+#         return False
+
+
+class GiftState(StatesGroup):
+    WaitingForPhoneNumber = State()
+
+
+class Review(StatesGroup):
+    WaitingForReview = State()
+
+
+def column_number_to_letter(column_number):
+    """Преобразует числовой индекс столбца в буквенный индекс."""
+    result = ""
+    while column_number > 0:
+        remainder = (column_number - 1) % 26
+        result = chr(65 + remainder) + result
+        column_number = (column_number - 1) // 26
+    return result
 
 
 @dp.message_handler(Command('start'))
@@ -31,16 +71,68 @@ async def support_request2(message: Message):
     keyboard.add(keys.ikb('Качество товара', 'ask_quality_product'),
                  keys.ikb('Комплектация товара', 'ask_equipment_product'),
                  keys.ikb('Другой вопрос', 'ask_any_question'))
-    await message.answer(text='<b>Выбери пожалуйста категорию обращения: </b>', reply_markup=keyboard, parse_mode='html')
+    await message.answer(text='<b>Выбери пожалуйста категорию обращения: </b>', reply_markup=keyboard,
+                         parse_mode='html')
 
 
 @dp.message_handler(text='Получить подарок')
 async def get_gift(message: Message):
     keyboard = keys.share_phone_markup()
     await bot.send_message(chat_id=message.from_user.id, text=f'{message.from_user.full_name}\n'
-                                'У нас для тебя подарок за отзыв. \n\n'
-                                'Пожалуйста, введи в ответном сообщении свой номер телефона, на него мы отправим бонус!\n'
-                                'Или нажми на кнопку «Поделиться номером»', reply_markup=keyboard)
+                                                              'У нас для тебя подарок за отзыв. \n\n'
+                                                              'Пожалуйста, введи в ответном сообщении свой номер телефона, на него мы отправим бонус!\n'
+                                                              'Или нажми на кнопку «Поделиться номером»',
+                           reply_markup=keyboard)
+    await GiftState.WaitingForPhoneNumber.set()
+
+
+@dp.message_handler(text='Отсчет')
+async def get_info_logs(message: Message):
+    if message.from_user.id in config.ADMINS:
+        logs = functions.get_info_logs()
+        rows = functions.get_answers_logs()
+        requests_value = logs[0]
+        answers_value = logs[1]
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+
+        sheet['A1'] = 'Дата'
+        sheet['B1'] = 'Сообщение'
+        sheet['C1'] = 'Пользователь'
+        sheet['H1'] = "Всего обращений"
+        sheet['I1'] = "Ответом менеджера"
+
+        start_row = 2  # Начинаем с 2, так как 1 строка обычно используется для заголовков
+
+        for index, value in enumerate((rows[0:][1][0]), start=2):
+            sheet[f'A{index}'] = value
+        for index, value in enumerate(rows[0:][1], start=2):
+            sheet[f'B{index}'] = value
+        for index, value in enumerate(rows[0:][2], start=2):
+            sheet[f'C{index}'] = value
+
+        sheet['I2'] = requests_value
+        sheet['H2'] = answers_value
+
+        # Проверяем условие и подсвечиваем ячейку
+        if requests_value < answers_value:
+            sheet['I2'].fill = openpyxl.styles.PatternFill(start_color='FF0000', end_color='FF0000',
+                                                           fill_type='solid')  # Красный
+        elif requests_value == answers_value:
+            sheet['I2'].fill = openpyxl.styles.PatternFill(start_color='00FF00', end_color='00FF00',
+                                                           fill_type='solid')  # Зеленый
+        # Сохранение файла
+        wb.save('example.xlsx')
+
+        with open('example.xlsx', 'rb') as excel_file:
+            await message.reply_document(excel_file)
+
+
+
+@dp.message_handler(text='Очистить таблицу')
+async def clear_logs(message: Message):
+    if message.from_user.id in config.ADMINS:
+        pass
 
 
 @dp.message_handler(Command('close'))
@@ -67,28 +159,39 @@ async def handle_message(message: Message):
         await message.answer("У вас нет прав на выполнение этой команды.")
 
 
-@dp.message_handler(content_types=ContentTypes.CONTACT)
-async def handle_contact(message: Message):
+@dp.message_handler(state=GiftState.WaitingForPhoneNumber, content_types=ContentTypes.TEXT)
+async def handle_phone_number(message: Message, state: FSMContext):
+    if message.text.isdigit() and len(message.text) == 11:
+        phone_number = message.text
+        print(phone_number)
+        await message.answer(f"Ты ввел номер телефона: {phone_number}\n\n"
+                             "Пожалуйста, подтверди, что номер верный:",
+                             reply_markup=keys.get_confirmation_keyboard(phone_number))
+        await state.finish()
+    else:
+        await message.answer("Пожалуйста, введи корректный номер телефона.")
+
+
+@dp.message_handler(content_types=ContentTypes.CONTACT, state=GiftState.WaitingForPhoneNumber)
+async def handle_contact1(message: Message):
     phone_number = message.contact.phone_number
-    print(phone_number)
     await message.answer(f"Ты поделился номером: {phone_number}\n\n"
                          "Пожалуйста, подтверди, что номер верный:",
                          reply_markup=keys.get_confirmation_keyboard(phone_number))
 
 
-@dp.message_handler(content_types=ContentTypes.PHOTO, state='*')
+@dp.message_handler(content_types=ContentTypes.TEXT, state=Review.WaitingForReview)
+async def handle_photo(message: Message):
+    await message.answer('Нам нужен только скриншот.')
+
+
+@dp.message_handler(content_types=ContentTypes.PHOTO, state=Review.WaitingForReview)
 async def handle_photo(message: Message, state: FSMContext):
-    data = await state.get_data()
-    confirm = data.get('confirm', False)
-    if not confirm:
-        await message.answer("Сначала подтвердите отправку отзыва кнопкой 'Поделиться отзывом'.")
-        return
-    await message.answer(f"💬Супер, спасибо!"
-                         "Скоро мы все проверим. Ответ поступит в этот бот - не останавливайте его.")
     await state.update_data(screenshot=message.photo[-1])
     photo = message.photo[-1]
     phone = functions.get_user_phone(message.from_user.id)
-    await bot.send_photo(chat_id=config.GROUP_ID, photo=photo.file_id, caption=f'{message.from_user.id}')
-    await bot.send_message(chat_id=config.GROUP_ID,
-                           text=f'Нужно проверить этот отзыв и начислить деньги на номер {phone}\n\n',
-                           reply_markup=keys.get_feedback_confirmation_keyboard())
+    await bot.send_photo(chat_id=config.GROUP_ID, photo=photo.file_id,
+                         caption=f'{message.from_user.id}\nНужно проверить этот отзыв и начислить деньги на номер <b>{phone}</b>',
+                         reply_markup=keys.get_feedback_confirmation_keyboard(), parse_mode='html')
+    await message.answer('Ваш скриншот успешно отправлен на модерацию ✅', reply_markup=keys.main_markup())
+    await state.finish()
