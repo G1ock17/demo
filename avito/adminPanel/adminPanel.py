a17e08e04dfbe60eaecca50374792546a83520a9
G1ock17 <109388456+G1ock17@users.noreply.github.com>
2024-06-07 17:51:37 +0300

    Add files via upload

--- avito/adminPanel/adminPanel.py
+++ avito/adminPanel/adminPanel.py
@@ -59,41 +59,118 @@ async def support_request(callback: CallbackQuery):
 
 @dp.callback_query_handler(lambda c: c.data == 'req_payment')
 async def support_request(callback: CallbackQuery):
-    keyboard = ikb_user('out_money')
+    keyboard = ikb_user('money')
     if keyboard:
         await callback.message.edit_reply_markup(reply_markup=keyboard)
     else:
         await callback.message.answer(text=f'У вас нет материала')
 
 
-@dp.callback_query_handler(lambda c: c.data.startswith('req_'))
-async def process_product_selection(callback_query: CallbackQuery):
-    req_type = callback_query.data.split('_')[1]
-    type_id = callback_query.data.split('_')[2]
-    print(type_id)
-    if req_type == 'material':
-        pass
-    elif req_type == 'anc':
-        pass
-    elif req_type == 'payment':
-        try:
-            description = functions.get_application(type_id)
+@dp.callback_query_handler(lambda c: c.data.startswith('adm_'))
+async def process_product_selection(callback: CallbackQuery):
+    req_type = callback.data.split('_')[1]
+    type_id = callback.data.split('_')[2]
 
+    if req_type == 'requests':
+        try:
+            description = functions.admin_get_info_req(req_type, type_id)
+
+            for request in description:
+                request_id = request[0]
+                user_id = request[1]
+                date = request[2]
+                status = request[3]
+
+                keyboard = InlineKeyboardMarkup()
+                keyboard.add(keys.ikb('👍', f'accept_{req_type}_{request_id}'),
+                             keys.ikb('👎', f'cancel_{req_type}_{request_id}'))
+
+                await callback.message.answer(
+                    text=f'Заявка №{request_id}\n\n'
+                         f'User: <b>{user_id}</b>\n'
+                         f'Дата: <b>{date[:16]}</b>\n', reply_markup=keyboard,
+                    parse_mode='html'
+                )
+        except Exception as e:
+            print(f"Произошла ошибка при обработке заявки: {e}")
+
+    if req_type == 'anc':
+        try:
+            description = functions.admin_get_info_req(req_type, type_id)
+
+            for request in description:
+                request_id = request[0]
+                user_id = request[1]
+                date = request[2]
+                link = request[3]
+                status = request[4]
+
+                keyboard = InlineKeyboardMarkup()
+                keyboard.add(keys.ikb('👍', f'accept_{req_type}_{request_id}'),
+                             keys.ikb('👎', f'cancel_{req_type}_{request_id}'))
+
+                await callback.message.answer(
+                    text=f'Заявка на проврку объявления №{request_id}\n\n'
+                         f'User: <b>{user_id}</b>\n'
+                         f'Дата: <b>{date[:16]}</b>\n'
+                         f'Ссылка на объявление: <b>{link}</b>\n', reply_markup=keyboard,
+                    parse_mode='html'
+                )
+
+        except Exception as e:
+            print(f"Произошла ошибка при обработке заявки: {e}")
+
+    if req_type == 'money':
+        try:
+            description = functions.admin_get_info_req(req_type, type_id)
             for request in description:
                 request_id = request[0]
                 requisite = request[1]
                 value = request[3]
                 date = request[4]
                 status = request[5]
-                status_text = "На проверке" if status == 0 else status
 
-                await callback_query.message.answer(
+                keyboard = InlineKeyboardMarkup()
+                keyboard.add(keys.ikb('👍', f'accept_{req_type}_{request_id}'),
+                             keys.ikb('👎', f'cancel_{req_type}_{request_id}'))
+
+                await callback.message.answer(
                     text=f'Заявка №{request_id}\n\n'
                          f'Сумма: <b>{value}</b>р\n'
                          f'Реквизиты: <b>{requisite}</b>\n'
-                         f'Дата: <b>{date[:16]}</b>\n'
-                         f'Статус: <b>{status_text}</b>\n',
+                         f'Дата: <b>{date[:16]}</b>\n', reply_markup=keyboard,
                     parse_mode='html'
                 )
+
         except Exception as e:
             print(f"Произошла ошибка при обработке заявки: {e}")
+
+
+@dp.callback_query_handler(lambda callback: callback.data.startswith(('accept_', 'cancel_')))
+async def handle_confirmation(callback: CallbackQuery):
+    action, req_type, type_id = callback.data.split('_')
+
+    if action == "accept":
+        user_id = functions.update_status_and_get_user_id(req_type, type_id, 2)
+        if req_type == 'anc':
+            text = 'Ваше объявление одобренно, на ваш счет зачисленно <b>300р</b>.'
+            functions.update_user_balance(user_id, 300, '+')
+            await bot.send_message(user_id, text, reply_markup=keys.main_markup(), parse_mode='html')
+        if req_type == 'money':
+            # функция для снятия денег со счета
+            text = 'Ваша заявка на вывод средств одобренна.'
+            await bot.send_message(user_id, text, reply_markup=keys.main_markup(), parse_mode='html')
+        if req_type == 'requests':
+            # функция для добавления материала для пользователя
+            functions.give_material_user(user_id)
+            text = 'Ваша заявка на получение материала одобренна, вы можете посмотреть свой материал в разделе <b>Мой материал</b>'
+            await bot.send_message(user_id, text, reply_markup=keys.main_markup(), parse_mode='html')
+
+
+    if action == "cancel":
+        if req_type == 'anc':
+            text = 'Ваше объявление одобренно, на ваш счет зачисленно <b>300р</b>.'
+        if req_type == 'money':
+            text = 'Ваша заявка на вывод средств одобренна.'
+        if req_type == 'requests':
+            text = 'Ваша заявка на вывод материала одобренна, вы можете посмотреть свой материал в разделе <b>Мой материал</b>'
