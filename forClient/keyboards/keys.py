e1c97b3b65d14327be70722b867117d82a625489
G1ock17 <109388456+G1ock17@users.noreply.github.com>
2024-06-07 17:52:42 +0300

    Add files via upload

--- forClient/keyboards/keys.py
+++ forClient/keyboards/keys.py
@@ -1,15 +1,9 @@
 from aiogram.types import KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
-back_message = '👈 Назад'
-confirm_message = '✅ Подтвердить'
-all_right_message = '✅ Все верно'
-cancel_message = '🚫 Отменить'
 
-
-def confirm_markup():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.add(confirm_message)
-    markup.add(back_message)
+def cancel():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton('Отмена'))
     return markup
 
 
@@ -18,43 +12,29 @@ def share_phone_markup():
     markup.add(KeyboardButton('Поделиться номером', request_contact=True))
     return markup
 
-def check_markup():
+
+def main_markup():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.row(back_message, all_right_message)
+    markup.row('Мой профиль', 'Мои заявки', 'Инструкция')
+    markup.row('Взять материал', 'Мой материал', 'Проверить объявление')
+    markup.row('Вывести средства', 'Написать в поддержку', 'Изменить реквезиты')
     return markup
 
 
-def submit_markup():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.row(cancel_message, all_right_message)
-    return markup
-
-
-def defolt_marup():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-    markup.row('Написать продавцу')
-
-
-def get_phone_request_keyboard():
+def admin_menu():
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("Поделиться номером", request_contact=True))
+    keyboard.add(InlineKeyboardButton("Заявки на материал", callback_data=f"req_material"),
+                 InlineKeyboardButton("Проверка объявлений", callback_data=f"req_anc"),
+                 InlineKeyboardButton("Заявки на выплату", callback_data=f"req_payment"))
     return keyboard
 
 
-def get_confirmation_keyboard(phone_number):
+def confirmation_keyboard_for_out_money():
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("Все верно 👍", callback_data=f"confirm_{phone_number}"),
-                 InlineKeyboardButton("Перепишу ❌", callback_data=f"rewrite_{phone_number}"))
+    keyboard.add(InlineKeyboardButton("Подтвердить 👍", callback_data=f"outmoney_confirm"),
+                 InlineKeyboardButton("Перепишу ❌", callback_data=f"outmoney_rewrite"))
     return keyboard
 
-
-def get_feedback_confirmation_keyboard():
-    keyboard = InlineKeyboardMarkup()
-    keyboard.row(
-        InlineKeyboardButton("Бонус отправлен ✅", callback_data='bonus_sent'),
-        InlineKeyboardButton("Отклонить ❌", callback_data='reject')
-    )
-    return keyboard
 
 def ikb(text, callback):
     button = InlineKeyboardButton(text=text, callback_data=callback)
